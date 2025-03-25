b72788d140c25cfbe2a24d66a7e13efc28beb742
thanhcd <chauducthanh02@gmail.com>
2025-03-25 22:07:59 +0700

    Feat: update Số điện thoại

--- app/(root)/(navigations)/(profile)/(info)/phone.tsx
+++ app/(root)/(navigations)/(profile)/(info)/phone.tsx
@@ -1,10 +1,31 @@
 import { View, Text, SafeAreaView, TouchableOpacity, Image, TextInput } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { router } from 'expo-router'
 import icons from '@/constants/icons'
 import CustomButton from '@/components/CustomButton'
+import { useGlobalContext } from '@/lib/GlobalProvider'
+import { updateProfileField } from '@/lib/appwrite'
 
 const phone = () => {
+  const { user } = useGlobalContext();
+
+  const userIdAuth: string = user?.$id || "";
+  const [phone, setPhone] = useState("");
+
+  const handleSavePhone = async () => {
+      if (!phone.trim()) {
+          alert("Vui lòng nhập số điện thoại!");
+          return;
+      }
+  
+      try {
+          await updateProfileField(userIdAuth, "Phone", phone.trim());
+          alert("Cập nhật số điện thoại thành công!");
+      } catch (error) {
+          alert("Cập nhật số điện thoại không thành công!");
+          console.error("Lỗi khi cập nhật số điện thoại:", error);
+      }
+  };
   return (
     <SafeAreaView className="h-full bg-white flex-1">
       <View className="flex-1 px-7 pb-5">
@@ -14,7 +35,7 @@ const phone = () => {
             <Image source={icons.left} className="w-9 h-9 mr-2" />
           </TouchableOpacity>
           <Text className="text-xl font-poppins-bold text-primary-200">
-            Email
+            Số điện thoại
           </Text>
         </View>
         <View className='flex-1 flex-col gap-5'>
@@ -27,12 +48,13 @@ const phone = () => {
               keyboardType="phone-pad"
               placeholder="(307) 555-0133"
               placeholderTextColor="#A0AEC0"
+              onChangeText={setPhone}
               className="flex-1 text-gray-200 text-base font-poppins-regular font-extrabold"
             />
           </View>
         </View>
         <View className='mt-auto pb-5'>
-          <CustomButton title='Save' handlePress={{}} containerStyles='bg-primary-100 rounded-lg' textStyles='text-white' />
+          <CustomButton title='Save' handlePress={handleSavePhone} containerStyles='bg-primary-100 rounded-lg' textStyles='text-white' />
         </View>
       </View>
     </SafeAreaView>
