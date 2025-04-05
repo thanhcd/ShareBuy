9509c43cb38020c695adc207e8aeb78d3c26739b
thanhcd <chauducthanh02@gmail.com>
2025-04-05 15:50:58 +0700

    Feat: setup function delete credit

--- app/(root)/(navigations)/(profile)/(payment)/credit.tsx
+++ app/(root)/(navigations)/(profile)/(payment)/credit.tsx
@@ -44,6 +44,19 @@ const Credit = () => {
         }
     }, [userIdAuth]);
 
+    const handleDeleteCredit = async (documentId: string) => {
+        try {
+
+        } catch (error) {
+            console.error('Lỗi khi xóa credit:', error);
+            Alert.alert('Lỗi', 'Đã xảy ra lỗi khi xóa credit');
+        }
+    }
+
+    const handleNotdone = () => {
+        Alert.alert('Thông báo', 'Chức năng này chưa được thực hiện.');
+    }
+
     return (
         <SafeAreaView className="h-full bg-white flex-1">
             <View className="flex-1 px-7 pb-5">
@@ -85,6 +98,7 @@ const Credit = () => {
                                 })}
                                 color={{}}
                                 bgColor={bgColor}
+                                handlePress={handleNotdone}
                             />
                         );
                     }}
